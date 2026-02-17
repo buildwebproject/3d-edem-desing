44712ffc3a4c8ee584eb2854ecfa99a4449bb321
Dev Patel <webifydev.team@gmail.com>
2026-02-17 15:37:44 +0530

    added updated code

--- assets/js/main.js
+++ assets/js/main.js
@@ -846,7 +846,7 @@ function initFilterDropdowns() {
 
     const rect = parts.button.getBoundingClientRect();
     const viewportPadding = 12;
-    const gap = 8;
+    const gap = 9;
 
     const menuWidth = parts.menu.offsetWidth || 320;
     const desiredHeight = parts.menu.scrollHeight || 0;
